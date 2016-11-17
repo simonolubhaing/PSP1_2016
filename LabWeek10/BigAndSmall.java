6c465b704599eaeb3b83e00ccebf4a62ddbbdc8c
simonolubhaing <simon.o.lubhaing@gmail.com>
2016-11-17 18:53:11 +0000

    Test Classes

---
+++ LabWeek10/BigAndSmall.java
@@ -0,0 +1,13 @@
+
+public class BigAndSmall {
+
+	public int findBiggest(int[] numbers)
+	{
+		return 0;
+	}
+
+	public int findSmallest(int[] numbers)
+	{
+		return 0;
+	}
+}
---
+++ LabWeek10/BigAndSmallTest.java
@@ -0,0 +1,21 @@
+import org.junit.Test;
+
+
+public class BigAndSmallTest {
+
+	private int[] testNumbersInOrder = {1,3,5,7,9,11};
+	private int[] testNumbersNotInOrder = {11,5,3,1,7};
+	private int[] testNumbersEmpty = {};
+	
+	@Test
+	public void testFindBiggest()
+	{
+		
+	}
+	@Test
+	public void testFindSmallest()
+	{
+		
+	}
+
+}
